341133b5f644c62379da8e1a132bcafb7fb1a38a
ZX-11 <63094790+ZX-11@users.noreply.github.com>
2023-11-18 15:04:32 +0800

    update

--- autologin.c
+++ autologin.c
@@ -1,16 +1,34 @@
+// Compile:       cc -s autologin.c -Ofast -o autologin 
+// Windows mingw: cc -s autologin.c -Ofast -o autologin -lwsock32 -fexec-charset=gbk
+
 #include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
+
+#ifdef __WIN32
+#include <WinSock2.h>
+#define close(s) closesocket(s)
+#define sleep(s) Sleep(1000*s)
+#pragma comment(lib,"ws2_32")
+typedef SOCKET socket_t;
+static __attribute__((constructor)) void _init() {
+	WSADATA wsa_data;
+	WSAStartup(MAKEWORD(2, 2), &wsa_data);
+}
+static __attribute__((destructor)) void _clean() { WSACleanup(); }
+#else
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+typedef int socket_t;
+#endif
 
 char* GET(char* hostname, char* path) {
 	static char buf[4096];
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
 	assert(sock != -1);
 	struct sockaddr_in server = {
 		.sin_addr.s_addr = inet_addr(hostname),
@@ -54,7 +72,7 @@ int main(int argc, char* argv[]) {
 				"login?callback=dr1003&DDDDD=%s&upass=%s&0MKKey=123456&"
 				"R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang="
 				"zh-cn&jsVersion=4.2.1&v=8321&lang=zh",
-				argv[1], urlencode(argv[2]));
+				argv[1], urlencode((unsigned char*)argv[2]));
 			time_t t = time(NULL);
 			fputs(ctime(&t), stdout);
 			puts((result = strstr(GET("10.10.43.3", buf), "result")) && result[8] == '1' ? "自动登录成功" : "登录失败");
